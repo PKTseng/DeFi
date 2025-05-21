9bae0f4a52d64768830d8d94aef2b66dbc51f766
PKTseng <jk0910948659@gmail.com>
2025-05-21 23:51:32 +0800

    add global api

---
+++ src/api/global.ts
@@ -0,0 +1,6 @@
+import axiosIns from '@/utils/request'
+import type { GlobalList } from '@/types/global'
+
+export const getGlobalList = async (): Promise<GlobalList> => {
+  return await axiosIns.get('/api/v3/global')
+}
