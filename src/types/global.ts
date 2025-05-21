1e6a3a517575e489ab5a3474247f9542a5102d85
PKTseng <jk0910948659@gmail.com>
2025-05-22 01:12:26 +0800

    優化 type

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,79 +1,9 @@
 export interface CurrencyData {
-  btc: number
-  eth: number
-  ltc: number
-  bch: number
-  bnb: number
-  eos: number
-  xrp: number
-  xlm: number
-  link: number
-  dot: number
-  yfi: number
-  usd: number
-  aed: number
-  ars: number
-  aud: number
-  bdt: number
-  bhd: number
-  bmd: number
-  brl: number
-  cad: number
-  chf: number
-  clp: number
-  cny: number
-  czk: number
-  dkk: number
-  eur: number
-  gbp: number
-  gel: number
-  hkd: number
-  huf: number
-  idr: number
-  ils: number
-  inr: number
-  jpy: number
-  krw: number
-  kwd: number
-  lkr: number
-  mmk: number
-  mxn: number
-  myr: number
-  ngn: number
-  nok: number
-  nzd: number
-  php: number
-  pkr: number
-  pln: number
-  rub: number
-  sar: number
-  sek: number
-  sgd: number
-  thb: number
-  try: number
-  twd: number
-  uah: number
-  vef: number
-  vnd: number
-  zar: number
-  xdr: number
-  xag: number
-  xau: number
-  bits: number
-  sats: number
+  [key: string]: number
 }
 
 export interface MarketCapPercentage {
-  btc: number
-  eth: number
-  usdt: number
-  xrp: number
-  bnb: number
-  sol: number
-  usdc: number
-  doge: number
-  ada: number
-  trx: number
+  [key: string]: number
 }
 
 export interface GlobalListData {
