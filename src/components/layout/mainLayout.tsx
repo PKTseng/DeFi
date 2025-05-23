ccc789edf32a70ec73bd1357e9a1671558ea7b1b
PKTseng <jk0910948659@gmail.com>
2025-05-23 20:31:21 +0800

    移除動畫效果

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,26 +1,13 @@
 import Header from './Header'
 import Footer from './Footer'
-import { Outlet, useLocation } from 'react-router-dom'
-import { AnimatePresence, motion } from 'framer-motion'
+import { Outlet } from 'react-router-dom'
 
 function MainLayout() {
-  const location = useLocation()
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 w-full bg-gray-900">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, x: 64 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -64 }}
-            transition={{ duration: 0.32, ease: 'easeInOut' }}
-            className="h-full"
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
+        <Outlet />
       </main>
       <Footer />
     </div>
