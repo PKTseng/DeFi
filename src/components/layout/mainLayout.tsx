d43aaa1d535dfc3f55dd231d79381053566cf685
PKTseng <jk0910948659@gmail.com>
2025-05-23 20:29:41 +0800

    加入動畫效果

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,13 +1,26 @@
 import Header from './Header'
 import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 
 function MainLayout() {
+  const location = useLocation()
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 w-full bg-gray-900">
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, x: 64 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -64 }}
+            transition={{ duration: 0.32, ease: 'easeInOut' }}
+            className="h-full"
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
       <Footer />
     </div>
