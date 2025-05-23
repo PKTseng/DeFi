a42199f271c359923e01aca75dd60613b0eb63d9
PKTseng <jk0910948659@gmail.com>
2025-05-23 20:02:52 +0800

    add format

---
+++ src/utils/helper.ts
@@ -0,0 +1,20 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatNumber(num: number): string {
+  if (num >= 1e12) {
+    return (num / 1e12).toFixed(2) + 'T'
+  } else if (num >= 1e9) {
+    return (num / 1e9).toFixed(2) + 'B'
+  } else if (num >= 1e6) {
+    return (num / 1e6).toFixed(2) + 'M'
+  } else if (num >= 1e3) {
+    return (num / 1e3).toFixed(2) + 'K'
+  } else {
+    return num.toFixed(2)
+  }
+}
