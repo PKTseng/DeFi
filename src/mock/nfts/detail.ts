7c8282ecdb97977465a43f7a0b8b2d435d46a8f3
PKTseng <jk0910948659@gmail.com>
2025-05-23 20:29:19 +0800

    add detail mock data

---
+++ src/mock/nfts/detail.ts
@@ -0,0 +1,99 @@
+export const nftDetail = {
+  id: 'autoglyphs',
+  web_slug: 'autoglyphs',
+  contract_address: '0xd4e4078ca3495de5b1d4db434bebc5a986197782',
+  asset_platform_id: 'ethereum',
+  name: 'Autoglyphs',
+  symbol: '☵',
+  image: {
+    small: 'https://coin-images.coingecko.com/nft_contracts/images/437/small/autoglyphs.png?1707287318',
+    small_2x: 'https://coin-images.coingecko.com/nft_contracts/images/437/small_2x/autoglyphs.png?1707287318',
+  },
+  banner_image: null,
+  description:
+    'Autoglyphs are the first “on-chain” generative art on the Ethereum blockchain. A completely self-contained mechanism for the creation and ownership of an artwork.',
+  native_currency: 'ethereum',
+  native_currency_symbol: 'ETH',
+  market_cap_rank: 5,
+  floor_price: {
+    native_currency: 112,
+    usd: 294944,
+  },
+  market_cap: {
+    native_currency: 57344,
+    usd: 151011400,
+  },
+  volume_24h: {
+    native_currency: 0,
+    usd: 0,
+  },
+  floor_price_in_usd_24h_percentage_change: 3.41751,
+  floor_price_24h_percentage_change: {
+    usd: 3.417507956101065,
+    native_currency: 0,
+  },
+  market_cap_24h_percentage_change: {
+    usd: 3.417507956101075,
+    native_currency: 0,
+  },
+  volume_24h_percentage_change: {
+    usd: 0,
+    native_currency: 0,
+  },
+  number_of_unique_addresses: 171,
+  number_of_unique_addresses_24h_percentage_change: 0,
+  volume_in_usd_24h_percentage_change: 0,
+  total_supply: 512,
+  one_day_sales: 0,
+  one_day_sales_24h_percentage_change: 0,
+  one_day_average_sale_price: null,
+  one_day_average_sale_price_24h_percentage_change: 0,
+  links: {
+    homepage: 'https://www.larvalabs.com/autoglyphs',
+    twitter: null,
+    discord: '',
+  },
+  floor_price_7d_percentage_change: {
+    usd: 6.8537700743842915,
+    native_currency: 0,
+  },
+  floor_price_14d_percentage_change: {
+    usd: 21.106338134748857,
+    native_currency: -2.608695652173913,
+  },
+  floor_price_30d_percentage_change: {
+    usd: 85.95477566625985,
+    native_currency: 20.43010752688172,
+  },
+  floor_price_60d_percentage_change: {
+    usd: 53.1765208969631,
+    native_currency: 12.562814070351758,
+  },
+  floor_price_1y_percentage_change: {
+    usd: -53.5412374197042,
+    native_currency: -36,
+  },
+  explorers: [
+    {
+      name: 'Etherscan',
+      link: 'https://etherscan.io/token/0xd4e4078ca3495de5b1d4db434bebc5a986197782',
+    },
+    {
+      name: 'Ethplorer',
+      link: 'https://ethplorer.io/address/0xd4e4078ca3495de5b1d4db434bebc5a986197782',
+    },
+  ],
+  user_favorites_count: 473,
+  ath: {
+    native_currency: 567,
+    usd: 1023185,
+  },
+  ath_change_percentage: {
+    native_currency: -80.24691358024691,
+    usd: -71.17391143970595,
+  },
+  ath_date: {
+    native_currency: '2023-05-22T00:05:43.220Z',
+    usd: '2023-05-22T00:05:43.220Z',
+  },
+}
