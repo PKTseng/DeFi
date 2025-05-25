ce62efb86d32d97699f5458d154530be4911ec6e
PKTseng <jk0910948659@gmail.com>
2025-05-25 20:38:14 +0800

    add global component

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,10 +1,12 @@
 import Header from './Header'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
+import GlobalLoading from '@/components/ui/global-loading'
 
 function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen">
+      <GlobalLoading />
       <Header />
       <main className="flex-1 w-full bg-gray-900">
         <Outlet />
